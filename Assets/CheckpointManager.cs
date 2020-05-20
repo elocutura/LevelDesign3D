067d62f2800c3bab976337d7ee3e5388bd7e3142
elocutura <eriktenis96@gmail.com>
2020-05-20 17:05:47 +0200

    Added checkpoints

---
+++ Assets/CheckpointManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public static CheckpointManager singleton;
+
+    public KeyCode loadCheckpointKey;
+
+    Transform lastCheckpointReached;
+    Transform player;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<InertiaCC>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(loadCheckpointKey))
+            LoadCheckpoint();
+    }
+
+    public void newCheckpointReached(Transform position)
+    {
+        lastCheckpointReached = position;
+    }
+
+    public void LoadCheckpoint()
+    {
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.transform.position = lastCheckpointReached.transform.position;
+        player.transform.rotation = lastCheckpointReached.transform.rotation;
+
+    }
+
+}
